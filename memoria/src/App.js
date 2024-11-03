cb56c7bba8c67df89ae1d3ba050d00923029a7c5
Riku Yukimaru <waimaru0713@gmail.com>
2024-11-03 21:37:56 +0900

    ✨ タスクの削除と残りタスクの表示
    - 完了済みタスクの削除機能の追加
    - 残りのタスク数を表示する機能の追加

--- memoria/src/App.js
+++ memoria/src/App.js
@@ -3,16 +3,13 @@ import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "掃除をする", completed: true },
-    { id: 2, name: "洗濯をする", completed: false },
-    { id: 3, name: "料理をする", completed: false }
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const todoNameRef = useRef();
 
   const handleAddTodo = () => {
     const name = todoNameRef.current.value;
+    if (name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
     });
@@ -26,12 +23,17 @@ function App() {
     setTodos(newTodos);
   }
 
+  const handleCler = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  }
+
   return <div className="App">
     <TodoList todos={todos} toggleTodo={toggleTodo}/>
     <input type="text" ref={todoNameRef}/>
     <button onClick={handleAddTodo}>タスクを追加</button>
-    <button>完了したタスクの削除</button>
-    <div>残りのタスク:0</div>
+    <button onClick={handleCler}>完了したタスクの削除</button>
+    <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
   </div>;
 }
 
